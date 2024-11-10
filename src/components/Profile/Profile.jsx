e7f82953cd80540b77a7b2f5dc874ee8fc75bc59
Viacheslav Danylko <gamerdvv@gmail.com>
2024-11-10 22:03:37 +0200

    create Profile.jsx

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,31 @@
+import s from './Profile.module.css';
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className={s.container}>
+      <div>
+        <img src={image} alt={name} />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
