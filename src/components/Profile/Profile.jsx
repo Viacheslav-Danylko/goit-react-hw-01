113e048e3431bdf5868c45b61b5f52b6fc63535e
Viacheslav Danylko <gamerdvv@gmail.com>
2024-11-10 22:32:45 +0200

    create profile css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,25 +3,25 @@ import s from './Profile.module.css';
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
     <div className={s.container}>
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>{tag}</p>
-        <p>{location}</p>
+      <div className={s.wrapper}>
+        <img src={image} alt={name} className={s.avatar} />
+        <p className={s.name}>{name}</p>
+        <p className="description">{tag}</p>
+        <p className="description">{location}</p>
       </div>
 
-      <ul>
-        <li>
-          <span>Followers</span>
-          <span>{stats.followers}</span>
+      <ul className={s.stats}>
+        <li className={s.item}>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
-        <li>
-          <span>Views</span>
-          <span>{stats.views}</span>
+        <li className={s.item}>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
         </li>
-        <li>
-          <span>Likes</span>
-          <span>{stats.likes}</span>
+        <li className={s.item}>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
