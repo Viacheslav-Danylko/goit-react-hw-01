8928509f6911f6836fe07d191bdc65b0a0af4b6e
Viacheslav Danylko <gamerdvv@gmail.com>
2024-11-11 13:17:35 +0200

    create TransactionsHistory

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,28 @@
+import s from './TransactionHistory.module.css';
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className="s.table">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(item => {
+          return (
+            <tr key={item.id}>
+              <td>{item.type}</td>
+              <td>{item.amount}</td>
+              <td className={s.currency}>{item.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
